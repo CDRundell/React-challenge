7717698655dbdaadeffbc33ee9eda73d6310a499
Chris Rundell <cdrundell@icloud.com>
2022-06-26 13:36:01 +0100

    redux added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,12 @@ import dummyData from "../dummyData.json"; // To be replaced with your api respo
 import { Link } from 'react-router-dom';
 
 export const Home = () => {
+
   return (
     <>
       <h1>Space X Ships</h1>
       <div className="App" style={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", rowGap: "10px", columnGap: "20px"}}>
-        <Link to="info/" underline="none"> <Card image={dummyData.image} name={dummyData.name} home_port={dummyData.home_port} roles={dummyData.roles} />
-          {/* <Card image={dummyData.image} name={dummyData.name} home_port={dummyData.home_port} roles={dummyData.roles} /> */}
-        </Link>
+          <Link to="info/" underline="none"><Card image={dummyData.image} name={dummyData.name} home_port={dummyData.home_port} roles={dummyData.roles} /></Link>
       </div>
     </>
   );
